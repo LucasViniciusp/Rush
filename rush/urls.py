d44280ae4206862a7fe29fb59465c8e321ba5cb4
LucasViniciusp <lucasviinicius03@hotmail.com>
2022-04-18 21:38:04 -0300

    feat(core): add register route

--- rush/urls.py
+++ rush/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
+from rush import views
+
 
 router = routers.DefaultRouter()
+router.register(r'register', views.RegisterViewSet, basename='user')
 
 urlpatterns = [
     path('', include(router.urls)),
