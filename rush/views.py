54e1c7dc261e1088dc7035d5a9a4cb030f93c87b
LucasViniciusp <lucasviinicius03@hotmail.com>
2022-04-18 21:38:35 -0300

    feat(core): add register view

--- rush/views.py
+++ rush/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from __future__ import nested_scopes
+from rest_framework.response import Response
+from rest_framework import viewsets, status, mixins
+from rest_framework.viewsets import GenericViewSet
+
+from rush import serializers
+from rush import models
+
 
 # Create your views here.
+class RegisterViewSet(viewsets.generics.CreateAPIView, GenericViewSet):
+    queryset = models.User.objects.all()
+    serializer_class = serializers.RegisterUserSerializer
